ef977d437ee505953acf92387f797a0231113ad2
ZYONCursoseTerapias <63822472+ZYONCursoseTerapias@users.noreply.github.com>
2026-08-15 11:51:54 -0300

    Corrige rolagem ao mudar de página

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import { Routes, Route } from 'react-router-dom'
+import { useLayoutEffect } from 'react'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import Welcome from './pages/Welcome'
 import Registration from './pages/Registration'
 import Test from './pages/Test'
 import Results from './pages/Results'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+
+  useLayoutEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' })
+  }, [pathname])
+
+  return null
+}
+
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Welcome />} />
-      <Route path="/registro" element={<Registration />} />
-      <Route path="/teste" element={<Test />} />
-      <Route path="/resultado" element={<Results />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/registro" element={<Registration />} />
+        <Route path="/teste" element={<Test />} />
+        <Route path="/resultado" element={<Results />} />
+      </Routes>
+    </>
   )
 }
